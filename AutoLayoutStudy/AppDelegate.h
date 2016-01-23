f34b36132aa8bb78bf8a1d77f9ac41b1e06c8c61
Cloudox <cloudox@foxmail.com>
2016-01-23 18:51:05 +0800

    Initial Commit

---
+++ AutoLayoutStudy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayoutStudy
+//
+//  Created by Cloudox on 16/1/23.
+//  Copyright © 2016年 Cloudox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoLayoutStudy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayoutStudy
+//
+//  Created by Cloudox on 16/1/23.
+//  Copyright © 2016年 Cloudox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
